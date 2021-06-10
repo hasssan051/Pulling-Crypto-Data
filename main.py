f1bd9b6512a839f290e0cb2184cd1d1ecf33b3b8
iamzaidi <hassanzaidi4@gmail.com>
2021-06-11 03:42:34 +0500

    Saving data to CSV enabled
    Data can now be saved to a csv from a dataframe

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import pandas as pd
 
+import pandas as pd
 from pycoingecko import CoinGeckoAPI
 
-cg = CoinGeckoAPI()
 
 df = pd.DataFrame(columns=['name', 'usd', 'usd_24h_change', 'btc', 'btc_24h_change', 'last_updated_at'])
+cg = CoinGeckoAPI()
 
 data = cg.get_price(ids='ethereum', vs_currencies='usd,btc', include_market_cap='false', include_24hr_vol='false',
                     include_24hr_change='true', include_last_updated_at='true')
@@ -13,4 +13,6 @@ df = df.append(data['ethereum'], ignore_index=True)
 df['name'] = 'ethereum'
 df['last_updated_at'] = (pd.to_datetime(df['last_updated_at'], unit='s'))
 
+df.to_csv('ethereum_data.csv', index=False, encoding='utf-8')
 
+print("CSV file called \'ethereum_data.csv\' succesfully created")
